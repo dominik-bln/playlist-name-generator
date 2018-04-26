1cb18e189109d66a1c0b3588580536ae28aaee2f
Ludwig Weiss <ludwig.weiss@umusic.com>
2018-04-26 15:08:29 +0200

    Import csv fiel to es

---
+++ api/import_csv.py
@@ -0,0 +1,21 @@
+import json
+import elasticsearch
+from elasticsearch import helpers
+import pandas as pd
+
+
+def bulk_load(filename='~/Dokumente/isrc_lookup.csv'):
+    df1 = pd.read_csv(filename, sep='\t', chunksize=1000)
+    es = elasticsearch.Elasticsearch('http://localhost:9200')
+    for chunk in df1:
+        tmp = chunk.to_dict(orient="records")
+        actions = [{
+            '_index': 'isrcs',
+            '_type': 'track',
+            '_source': doc
+        } for doc in tmp]
+        helpers.bulk(es, actions)
+
+
+if __name__ == '__main__':
+    bulk_load()
